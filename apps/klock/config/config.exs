b6d6644013f6f9be94ca6abe1cf7ae4137b9e766
Zen Savona <z@zen.id.au>
2018-12-18 01:39:51 +0700

    0.4.3

--- apps/klock/config/config.exs
+++ apps/klock/config/config.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 
 config :klock,
-  interval: 1000
+  interval: 2000
--- rel/config.exs
+++ rel/config.exs
@@ -45,7 +45,7 @@ end
 # will be used by default
 
 release :umbrella_distillery_demo do
-  set version: "0.4.2"
+  set version: "0.4.3"
   set applications: [
     :runtime_tools,
     blog: :permanent,
